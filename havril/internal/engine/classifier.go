12a7fa058e0d09b027b2fb3929a1cbd21b2c901f
freedisch <freeproduc@gmail.com>
2026-04-07 00:24:42 +0200

    feat: add classifier and scorer
    Signed-off-by: freedisch <freeproduc@gmail.com>

---
+++ havril/internal/engine/classifier.go
@@ -0,0 +1,20 @@
+package engine
+
+import "github.com/freedisch/havril/pkg/models"
+
+var validTypes = map[string]bool{
+	models.MemoryTypeSemantic: true,
+	models.MemoryTypeEpisodic: true,
+	models.MemoryTypeProcedural: true,
+	models.MemoryTypeSummary: true,
+}
+
+type Classifier struct {}
+func newClassifier() *Classifier { return &Classifier{}}
+
+func(c *Classifier) classify(memType string) string {
+	if validTypes[memType]{
+		return memType
+	}
+	return models.MemoryTypeSemantic
+}
